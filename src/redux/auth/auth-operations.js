842f8acb88f4cc41269a4f008c3be1719990f4ca
Polina Dudchenko <pv.dudchenko@gmail.com>
2022-01-06 15:06:30 +0100

    netlify

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { token } from 'services/axious-settings'
 import * as userApi from 'services/user-api'
-import Alert from '@material-ui/Alert'
+import Alert from '@material-ui/lab/Alert'
 
 const register = createAsyncThunk('auth/register', async credentials => {
     try {
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,5 +1,19 @@
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(() => ({
+  homeTitle: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '30px',
+    width: '600px',
+    color: '#414141',
+
+  }
+}));
 export function HomePage() {
   
-  return <h2>Hi, here you can create your own contacts book</h2>;
+  const { homeTitle } = useStyles();
+  return <h3 className={homeTitle}>Hi, here you can create your own contact book. For a start, please log in or register</h3>;
 
 }
