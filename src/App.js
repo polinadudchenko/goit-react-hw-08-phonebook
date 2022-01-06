0d50d603e7b155fb4eaf8f1f87ee19ef8852f22a
Polina Dudchenko <pv.dudchenko@gmail.com>
2022-01-06 12:51:43 +0100

    make a loader for currentUser

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import PrivateRoute from 'components/PrivateRoutes';
 import PublicRoute from 'components/PublicRoutes';
+import { authSelectors, authOperations } from 'redux/auth';
 
 import { HomePage } from 'views/HomePage';
 import Layout from 'components/Layout';
@@ -15,30 +17,45 @@ const AsyncContactsPage = lazy(() => import('views/Contacts'));
 
 export default function App() {
   
+  const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
+  console.log(isFetchingCurrentUser);
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
 
-  return <Suspense
-      fallback={
-        <Loader
-          type="TailSpin"
-          color="#f19816"
-          height={100}
-          width={100}
-          timeout={3000} //3 secs
-          style={{ textAlign: 'center', padding: '100px' }}
-        />
-      }
-    >
-        <Routes>
+  return isFetchingCurrentUser ? <Loader
+              type="TailSpin"
+              color="#f19816"
+              height={100}
+              width={100}
+              timeout={3000} //3 secs
+              style={{ textAlign: 'center', padding: '100px' }}
+            />
+       :<Suspense
+          fallback={
+            <Loader
+              type="TailSpin"
+              color="#f19816"
+              height={100}
+              width={100}
+              timeout={3000} //3 secs
+              style={{ textAlign: 'center', padding: '100px' }}
+            />
+          }
+        >
+          <Routes>
             <Route path="*" element={<Layout />}>
               <Route index element={<HomePage />} />
-              <Route path='register' element={<PublicRoute redirectTo='/contacts' restricted> <AsyncRegistrationPage /> </PublicRoute> }/>
-              <Route path='login' element={<PublicRoute redirectTo='/contacts' restricted> <AsyncLoginPage /> </PublicRoute> }/>
-              <Route path='contacts' element={<PrivateRoute redirectTo='/login'><AsyncContactsPage /></PrivateRoute>}/>
+              <Route path='register' element={<PublicRoute redirectTo='/contacts' restricted> <AsyncRegistrationPage /> </PublicRoute>} />
+              <Route path='login' element={<PublicRoute redirectTo='/contacts' restricted> <AsyncLoginPage /> </PublicRoute>} />
+              <Route path='contacts' element={<PrivateRoute redirectTo='/login'><AsyncContactsPage /></PrivateRoute>} />
               <Route path="*" element={<Navigate to={'/'} />} />
             </Route>
-        </Routes>
-    </Suspense>
-} 
+          </Routes>
+        </Suspense>
+}
+
 
 
 
--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -2,8 +2,11 @@ const getIsLoggedIn = state => state.auth.isLoggedIn;
 
 const getUserName = state => state.auth.user.name;
 
+const getIsFetchingCurrent = state => state.auth.isFetchingCurrentUser;
+
 const authSelectors = {
   getIsLoggedIn,
   getUserName,
+  getIsFetchingCurrent
 };
 export default authSelectors;
